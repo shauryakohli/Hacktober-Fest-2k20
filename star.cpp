da7d78e6c767aea34d53be4ba45a89a9f6d56942
kaustubh-vats <kaustubhvats08@gmail.com>
2020-10-13 02:12:13 +0530

    Added serial number to each row, Handled row less than equal to zero error

--- star.cpp
+++ star.cpp
@@ -6,12 +6,14 @@ int main()
     int i, j, N;
 
     /* Input number of rows from user */
+    do{
     printf("Enter number of rows: ");
-    scanf("%d", &N);
+    scanf("%d", &N);}while(N<=0); //Numer of roes and column should be greater than 0
 
     /* Iterate through N rows */
     for(i=1; i<=N; i++)
     {
+        printf("%d. ",i); //Print number of rows
         /* Iterate over columns */
         for(j=1; j<=N; j++)
         {
